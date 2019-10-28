7a2f02bb5acfbee6845b0270aba4a49a429d289b
Duncan Ndiithi <duncanndiithi@yahoo.com>
2019-10-28 15:22:49 +0300

    bar graph drawer

---
+++ components/utils/IndicatorBarGraph.js
@@ -0,0 +1,61 @@
+import React, { PureComponent } from 'react';
+import { ConvertToMonthlyLineGraph } from './converters/Charts'
+import { FetchIndicatorData } from './Helpers'
+import {
+  BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
+
+
+class CustomizedLabel extends PureComponent {
+  render() {
+    const {
+      x, y, stroke, value,
+    } = this.props;
+
+    return <text x={x} y={y} dy={-4} fill={stroke} fontSize={10} textAnchor="middle">{value}</text>;
+  }
+}
+
+class CustomizedAxisTick extends PureComponent {
+  render() {
+    const {
+      x, y, stroke, payload,
+    } = this.props;
+
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <text x={5} y={0} dy={16} textAnchor="end" fill="#666" transform="rotate(-90)">{payload.value}</text>
+      </g>
+    );
+  }
+}
+
+
+export default class IndicatorLineBarGraph extends PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.state = {data: ''};
+  }
+
+  componentDidMount() {
+    (async () => {
+      let returnedData=await FetchIndicatorData(this.props.id,this.props.ouid,this.props.pe,null);
+      var _data=ConvertToMonthlyLineGraph(returnedData.indicatorData.result.data);
+       this.setState({data: _data});
+    })()
+  };
+
+  render() {
+    return (
+        <BarChart width={650} height={300} data={this.state.data} margin={{ top: 10, right: 20, left: 0, bottom: 5,}}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" tick={<CustomizedAxisTick />}/>
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar type="monotone" dataKey="value" fill="#82ca9d" label={<CustomizedLabel />} />
+        </BarChart>
+    );
+  }
+}
