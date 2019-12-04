e4c1a1b6f2c00d8ed1132a55443a723cbe140dbb
Duncan Ndiithi <duncanndiithi@yahoo.com>
2019-12-04 16:36:40 +0300

    line to bar chart

--- components/ServicesCoverageModal.js
+++ components/ServicesCoverageModal.js
@@ -108,7 +108,7 @@ export default class ServiceCoverage extends React.Component {
                 <div class="column ">
                 <div className="box m-5">
                   <h5 className="title m-b-0 m-l-10 is-6 fcprimary-dark text-caps text-center">% Diabetes screened for the period: {this.state.period}</h5>
-                  <IndicatorLineGraph id={342134} pe={this.state.period} ouid={this.state.ouid}/>
+                  <IndicatorLineGraph type={'column'} id={342134} pe={this.state.period} ouid={this.state.ouid}/>
                 </div>
                 </div>
             </div>
