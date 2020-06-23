4ac3615bde1783d355160623a94f6166af5d507d
Duncan Ndiithi <duncanndiithi@yahoo.com>
2020-06-23 09:57:15 +0300

    choropleth map

--- pages/Covid19.js
+++ pages/Covid19.js
@@ -4,16 +4,23 @@ import MapCenters from '../static/maps/county-centers-coordinates'
 import Layout from '../components/Layout'
 import Link from 'next/link';
 import SidePanel from '../components/pandemic/map/SidePanel'
-import {fetchCovidData} from '../components/utils/Helpers';
+import ChoroPlethLegend from '../components/pandemic/map/choroPlethLegend'
+import {fetchCovidData , insertCovidValues} from '../components/utils/Helpers';
+import CovidBubble from '../components/pandemic/map/covidBubble'
 
 export default class extends React.Component {
 
-  constructor () {
-    super()
+  constructor (props) {
+    super(props);
+        this.leafLetMap = React.createRef();
+
     this.state = {
       components: undefined,
       mapCentersData: MapCenters,
-      covidData: null
+      covidData: null,
+      defaultGeoJson: JSON.parse(JSON.stringify(MapData)),
+      choroPlethData: null,
+      choroPleathPopUpdIndicator: "confirmed cases"
     };
   }
 
@@ -21,8 +28,10 @@ export default class extends React.Component {
 
     (async () => { //http://dsl.health.go.ke/dsl/api/pandemics/covid19?id=6074&start_date=2020-06-07
       let {covidData}=await fetchCovidData(null,null,null,null);
+      let choroPlethData=insertCovidValues(covidData,MapData,6074, "confirmed cases"); //confirmed cases
       this.setState({
-        covidData: covidData
+        covidData: covidData,
+        choroPlethData: choroPlethData
       });
 
     })()
@@ -56,8 +65,16 @@ export default class extends React.Component {
         LayerGroup, Circle, Rectangle, BaseLayer, Overlay
       }
 
-    })
+    });
 
+  }
+
+  setCurrenGeoJsonToDisplay =(gJsonData)=>{
+    this.refs.coviGgeojson.leafletElement.clearLayers();
+    this.refs.coviGgeojson.leafletElement.addData(gJsonData);
+    this.setState({
+      choroPlethData: gJsonData,
+    });
   }
 
   geoJsonStyle = (feature)=>{
@@ -71,6 +88,96 @@ export default class extends React.Component {
             fontWeight: 700
         };
     };
+
+    choroplethStyle= (feature)=> {
+      return {
+        fillColor: this.getChoroplethColor(feature.properties.density),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+      };
+  }
+
+
+    getChoroplethColor =(d)=>{
+     return d==undefined ? '#ccdaee':
+            d==null ?  '#ccdaee':
+            d > 1000 ? '#800026' :
+            d > 500  ? '#BD0026' :
+            d > 200  ? '#E31A1C' :
+            d > 100  ? '#FC4E2A' :
+            d > 50   ? '#FD8D3C' :
+            d > 20   ? '#FEB24C' :
+            d > 10   ? '#FED976' :
+                       '#FFEDA0';
+     }
+
+    //on mouse hover
+   choroplethHighlightFeature= (e)=> {
+        //add toot tip to county
+        this.refs.coviGgeojson.leafletElement.bindTooltip(`
+            <div>${e.target.feature.properties.AREA_NAME}</div>
+            <div style='float: left'>${this.state.choroPleathPopUpdIndicator}:
+             ${e.target.feature.properties.density}</div>
+           `);
+
+        //highlight selected county
+        let layer = e.target;
+        layer.setStyle({
+            weight: 5,
+            color: '#666',
+            dashArray: '',
+            fillOpacity: 0.7
+        });
+        if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+            layer.bringToFront();
+        }
+
+    }
+
+    //on mouse out
+    choroplethResetHighlight= (e)=> {
+
+      this.refs.coviGgeojson.leafletElement.resetStyle(e.target);
+    }
+
+    choroplethOnEachFeatureAddListener =(feature, layer)=> {
+      layer.on({
+        mouseover: this.choroplethHighlightFeature.bind(this),
+        mouseout: this.choroplethResetHighlight.bind(this)
+      });
+    }
+
+    updateCovidData = (id, indicatorName)=>{
+
+        (async () => { //http://dsl.health.go.ke/dsl/api/pandemics/covid19?id=6074&start_date=2020-06-07
+          //let {covidData}=await fetchCovidData(null,null,null,null);
+          let choroPlethData=insertCovidValues(this.state.covidData,MapData,id,indicatorName); //confirmed cases
+
+          this.setState({
+            choroPlethData: choroPlethData
+          });
+
+        })()
+    }
+
+    addMarkersOnMap = (feature, layer)=>{
+
+      layer.on({
+        mouseover: this.choroplethHighlightFeature2.bind(this),
+      });
+
+    }
+
+    insertBubbleLayer=()=>{
+      console.log(this.refs.layersControl);
+      console.log('==== done')
+      var littleton = L.marker([0.166779241, 37.764037054]).bindPopup('This is Littleton, CO.');
+      this.refs.layersControl.leafletElement.addOverlay(littleton,"test layer");
+    //  L.control.layers(baseMaps, overlayMaps).addTo(map);
+    }
 
   render() {
 
@@ -96,12 +203,10 @@ export default class extends React.Component {
       [0.18, 37.79],
     ]
 
-
     const rectangle2 = [
-  [51.49, -0.08],
-  [51.5, -0.06],
-]
-
+      [51.49, -0.08],
+      [51.5, -0.06],
+    ]
 
     return (
     <Layout>
@@ -113,11 +218,17 @@ export default class extends React.Component {
               width: 100%;
               height: 80% !important;
             }
+            .leaflet-control-layers-toggle {
+	               background-image: url(../static/images/layers.png);
+            }
+
+            .leaflet-retina .leaflet-control-layers-toggle {
+            	background-image: url(../static/images/layers-2x.png);
+            	}
 
           `}
         </style>
       }
-
 
       {/* Breadcrumb */}
       <section className="section m-t-20 m-b-5 bcclear p-b-15">
@@ -139,18 +250,18 @@ export default class extends React.Component {
       </section>
       {/* Breadcrumb */}
 
-
       <div style={{minWidth: 500 + 'px', minHeight: 500 + 'px', height: 900 + 'px'}}>
+          <LeafletMap ref="covMap" scrollWheelZoom={false} center={center} zoom={6.5} maxZoom={18} >
 
-          <LeafletMap scrollWheelZoom={false} ref={node => {this.map = node }} center={center} zoom={6.5} maxZoom={18} >
-            <SidePanel covidData={this.state.covidData}/>
-             <GeoJSON data={MapData} key={MapData} style={this.geoJsonStyle} />
-             <LayersControl collapsed={false} position="topright">
-             <BaseLayer checked name="OpenStreetMap">
-               <TileLayer
-                 attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-               />
+            <SidePanel insertBubbleLayer={this.insertBubbleLayer} choroPlethData={this.state.choroPlethData} setCurrenGeoJsonToDisplay={this.setCurrenGeoJsonToDisplay} defaultGeoJson={this.state.defaultGeoJson} covidData={this.state.covidData}/>
+            <ChoroPlethLegend  getChoroplethColor={this.getChoroplethColor}/>
+             <GeoJSON ref="coviGgeojson" data={this.state.choroPlethData} key={this.state.choroPlethData} style={this.choroplethStyle} onEachFeature= {this.choroplethOnEachFeatureAddListener.bind(this)} />
+             <LayersControl ref='layersControl' collapsed={true} position="topright">
+               <BaseLayer checked name="OpenStreetMap">
+                 <TileLayer
+                   attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                 />
                </BaseLayer>
                <BaseLayer  name="OSM plain">
                  <TileLayer
@@ -176,45 +287,12 @@ export default class extends React.Component {
                    url="http://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}&s=Ga"
                  />
                </BaseLayer>
-               <Overlay name="Marker with popup">
-                 <Marker position={center}>
-                   <Popup>
-                     A pretty CSS3 popup. <br /> Easily customizable.
-                   </Popup>
-                 </Marker>
-               </Overlay>
 
-               <Overlay checked name="Layer group with circles">
-                 <LayerGroup>
-                   <Circle center={center} fillColor="blue" radius={8200} />
-                   <Circle
-                     center={center}
-                     fillColor="red"
-                     radius={100}
-                     stroke={false}
-                   />
-                   <LayerGroup>
-                     <Circle
-                       center={[51.51, -0.08]}
-                       color="green"
-                       fillColor="green"
-                       radius={100}
-                     />
-                   </LayerGroup>
-                 </LayerGroup>
-               </Overlay>
-               <Overlay name="Feature group">
-                 <FeatureGroup color="purple">
-                   <Popup>Popup in FeatureGroup</Popup>
-                   <Circle center={[51.51, -0.06]} radius={200} />
-                   <Rectangle bounds={rectangle} />
-                 </FeatureGroup>
-               </Overlay>
              </LayersControl>
-
           </LeafletMap>
 
         </div>
+
     </Layout>
     );
   }
