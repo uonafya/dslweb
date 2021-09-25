902a9acf54af98d88f0b887c91de30b049d5b553
Benzer Bett <benzerbett@gmail.com>
2021-09-25 08:58:39 +0300

    next styles fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-const withCSS = require('@zeit/next-css')
+const withCSS = require('next-plugin-antd-less')
 module.exports = withCSS({
   /* config options here */
 })
+// module.exports = withPlugins([...], {
+//    webpack5: false,
+// })
